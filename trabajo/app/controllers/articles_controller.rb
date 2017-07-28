26c4bd66e15b98d9d0ab8833e536a91980e07cfa
Marcos Sierra <msierr37@eafit.edu.co>
2017-07-27 22:23:44 -0500

    Login en página

--- trabajo/app/controllers/articles_controller.rb
+++ trabajo/app/controllers/articles_controller.rb
@@ -7,4 +7,21 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+    #GET /articles/new
+    def new
+        @article = Article.new
+    end
+    #POST /articles
+    def create
+        @article = Article.new(title: params[:article][:title], 
+                                body: params[:article][:body])
+        if  @article.save
+            redirect_to @article
+        else
+            render :new
+        end
+    end 
+    #PUT /articles/:id
+    def update
+    end
 end
--- trabajo/config/routes.rb
+++ trabajo/config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :articles
 =begin
-    get "/articles"
-    post "/articles"
-    delete "/articles"
-    get "/articles/:id"
-    get "/articles/new"
-    get "/articles/:id/edit"
-    patch "/articles/:id"
-    put "/articles/:id"
+    get "/articles" index
+    post "/articles" create
+    delete "/articles" delete
+    get "/articles/:id" show
+    get "/articles/new" new
+    get "/articles/:id/edit" edit
+    patch "/articles/:id" update
+    put "/articles/:id" update
 =end
   #get 'welcome/index'
   root 'welcome#index'
